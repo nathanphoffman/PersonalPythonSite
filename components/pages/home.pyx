227965bf2935f9db4a74983d477818c191091220
Nate <natephiliphoffman@proton.me>
2026-09-23 20:15:54 -0400

    updated header content on the homepage

--- components/pages/home.pyx
+++ components/pages/home.pyx
@@ -14,7 +14,7 @@ BLOG_BUTTON_CLASS = "inline-block px-10 py-3 text-base font-medium text-gray-700
 def Home(on_navigate=None) -> Any:
     return (
         <div>
-            <div className="flex items-center justify-between bg-blue-100 text-blue-800 px-4 py-3 rounded border border-blue-300">
+            <div className="flex items-center justify-between bg-blue-100 text-blue-800 px-4 py-3 rounded border border-blue-300 -mt-8 mb-6">
                 <p>This site was recently rewritten from an early version of React still on my GitHub to this form you see now I got rid of the Coding Astronomer domain.  It was done as an experiment to try to replicate a React-TS-JSX-like framework utilizing Python.  Since much of this site was based on original design work (which itself was based on another blog template I found online) and this has since been reworked manually with some claude help to move it over, it is a hodge podge of sourcing but mostly of my own design.</p>
             </div>
             
@@ -22,13 +22,12 @@ def Home(on_navigate=None) -> Any:
                         
 
     
-            <h1 className="text-4xl font-bold text-gray-900 mb-6">Current Projects</h1>
-            <p className="text-gray-700 leading-relaxed mb-8">
-                Welcome! Below you would find my latest posts on Medium. You can see my
-                <NavLink href="/projects" onNavigate={on_navigate} className="text-blue-600 hover:text-blue-800 mx-1">projects here</NavLink>
-                and access the about page at the links above.
-            </p>
             <h1 className="text-4xl font-bold text-gray-900 mb-6">Highlighted Projects</h1>
+            <p className="text-gray-700 leading-relaxed mb-6">
+                Below are a few of the projects I'm most excited about right now. You can see
+                <NavLink href="/projects" onNavigate={on_navigate} className="text-blue-600 hover:text-blue-800 mx-1">all of my projects here</NavLink>
+                or learn more about me on the about page at the links above. Further down you'll find my latest blog posts.
+            </p>
             <ProjectGrid>
                 <DeorLanguage />
                 <DeorPico />
